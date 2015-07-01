43807c0025ba4ab0f71a8cc0411ca95c317f4798
Lanz <zihao.lan@dianrong.com>
2015-07-01 18:40:32 +0800

    Ex 13

--- program.js
+++ program.js
@@ -173,3 +173,38 @@
 // });
 //
 // server.listen(process.argv[2]);
+
+// Ex 13
+// var http = require('http');
+// var url = require('url');
+//
+// var routes = {
+//	 '/api/parsetime': function(parseUrl) {
+//		 var d = new Date(parseUrl.query.iso);
+//		 return {
+//			 hour: d.getHours(),
+//			 minute: d.getMinutes(),
+//			 second: d.getSeconds()
+//		 };
+//	 },
+//	 '/api/unixtime': function(parseUrl) {
+//		 var d = new Date(parseUrl.query.iso);
+//		 return {
+//			 unixtime: d.getTime()
+//		 };
+//	 }
+// };
+//
+// var server = http.createServer(function(req, res) {
+//	 parseUrl = url.parse(req.url, true);
+//	 resource = routes[parseUrl.pathname];
+//	 if (resource) {
+//		 res.writeHead(200, { 'Content-type': 'application/json' });
+//		 res.end(JSON.stringify(resource(parseUrl)));
+//	 } else {
+//		 res.writeHead(404);
+//		 res.end();
+//	 }
+// });
+//
+// server.listen(process.argv[2]);
